9fc6a19d08f88b90fa476c683cdae920d5cd2d4d
DmitrievDV <13@13.tt>
2020-09-25 10:23:23 +0300

    Add project files.

---
+++ lotery/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lotery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chosenPool = ReadIntLine().Take(10);
+            var playersCount = ReadIntLine().First();
+            var playersPicks = new List<IEnumerable<int>>();
+            for (int i = 0; i < playersCount; i++)
+            {
+                playersPicks.Add(ReadIntLine().Take(6));
+            }
+          
+            foreach (var p in playersPicks)
+            {
+                Console.WriteLine(IsLucky(p, chosenPool) ? "Lucky" : "Unlucky");
+            }
+        }
+        static bool IsLucky(IEnumerable<int> check, IEnumerable<int> target)
+        {
+            return check.Intersect(target).Count() >= 3;
+        }
+        static IEnumerable<int> ReadIntLine()
+        {
+            return Console.ReadLine().Split(' ').Select(x => int.Parse(x));
+        }
+    }
+}
---
+++ palindromeFactor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace palindromeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = Console.ReadLine();
+            Console.WriteLine(FindMinimalPalindrome(source));
+        }
+        static string FindMinimalPalindrome(string source)
+        {
+            var lengths = Enumerable.Range(2, source.Length + 2);
+
+            foreach (int palindromeLength in lengths)
+            {
+                var extracted = ExtractPalindromes(source, palindromeLength);
+
+                var minimal = extracted.Min();
+                if (!string.IsNullOrEmpty(minimal))
+                    return minimal;
+            }
+            return "-1";
+        }
+        static IEnumerable<string> ExtractPalindromes(string source, int palindromeLength)
+        {
+            for (int i = 0; i <= source.Length - palindromeLength; i++)
+            {
+                var testStr = source.Slice(i, palindromeLength);
+                if (IsPalindrome(testStr))
+                    yield return new string(testStr.ToArray());
+            }
+        }
+        static bool IsPalindrome(IEnumerable<char> testTarget)
+        {
+            return testTarget.SequenceEqual(testTarget.Reverse());
+        }
+    }
+    static class IEnumerableSliceExtension
+    {
+        public static IEnumerable<T> Slice<T>(this IEnumerable<T> source, int start, int length)
+        {
+            return source.Skip(start).Take(length);
+        }
+    }
+}
