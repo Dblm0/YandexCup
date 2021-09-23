a33c16df967077a5ebe375a844b98048de11e565
Dmitriev <Dmitriev@DESKTOP-HNQRCVK>
2021-09-23 12:25:03 +0300

    that was a mistake

--- SnJ/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Linq;
-
-char[] J = Console.ReadLine().Distinct().ToArray();
-var S = Console.ReadLine()
-    .GroupBy(x => x)
-    .ToDictionary(x => x.Key, x => x.Count());
-
-Console.WriteLine($"{S.Where(x => J.Contains(x.Key)).Sum(x => x.Value)}");
