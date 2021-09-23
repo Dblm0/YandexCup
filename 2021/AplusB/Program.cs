7d8a5fdb30155ae333cfbf7e31ada97fce7c413a
Dmitriev <Dmitriev@DESKTOP-HNQRCVK>
2021-09-23 11:22:47 +0300

    start 2021 practice

---
+++ 2021/AplusB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplusB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
